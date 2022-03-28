2e8f97ac052ba553a3aba6ed05c56d11143cb70f
jassulan <jassulan.expo@gmail.com>
2022-03-28 14:23:21 -0400

    Adjust Facts page and Fix _obj_ error

--- src/views/Facts.vue
+++ src/views/Facts.vue
@@ -1,36 +1,70 @@
 <template>
 <div>
     <NavBar />
-    <h1>Facts</h1>
-
-
-<div style="display: inline-block';
-                position: absolute;
-                bottom: 5%;
-                width: 40%;
-                height: 60%;
-                left: 5%; border: 2px solid black;"> 
-    <h3> texts</h3>
+    <div >
+        <router-link to="/sites" class="links2"><u>Sites</u></router-link>
+        <router-link to="/" class="links1"><u>Home</u></router-link>
     </div>
+
+    <h3 style="margin-top: 90px;">Facts</h3>
+
+<div class="texts1"> 
+    <h3> texts</h3>
+</div>
     
-    <div style="display: inline-block';
-                position: absolute;
-                bottom: 5%;
-                width: 40%;
-                height: 60%;
-                left: 55%; border: 2px solid black;"> 
+<div class="texts2"> 
     <h3> texts</h3>
-    </div>
+</div>
+
+    <Footer />
 </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavBar from "@/components/NavBar.vue";
+import Footer from "@/components/Footer.vue";
 export default {
     name: "Facts",
     components: {
-        NavBar
+        NavBar,
+        Footer
     },
 };
 </script>
+<style>
+    .links2 {
+        text-align: center;
+        float: right; 
+        margin-right: 60px; 
+        margin: 30px; 
+        color: rgb(0, 0, 0);
+    }
+
+    .links1 {
+        text-align: center;
+        float: right; 
+        margin: 30px; 
+        color: rgb(0, 0, 0);
+    }
+
+    .texts1 {
+        display: inline-block;
+        position: absolute;
+        bottom: 10%;
+        width: 40%;
+        height: 60%;
+        left: 5%; 
+        border: 2px solid black;
+    }
+
+    .texts2 {
+        display: inline-block;
+        position: absolute;
+        bottom: 10%;
+        width: 40%;
+        height: 60%;
+        left: 55%; 
+        border: 2px solid black;
+    }
+</style>
