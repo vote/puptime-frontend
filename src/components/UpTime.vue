c783b3d90045c4f4e76c97527c56659a828cecbf
jassulan <jassulan.expo@gmail.com>
2022-04-26 15:42:58 -0400

    UpTime sorted

--- src/components/UpTime.vue
+++ src/components/UpTime.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="overflow-auto">
-    <b-table sticky-header :items="state_monthly_uptimes" head-variant="light">
-                <template #head(0)="">
-                    <span class="text-info">State</span>
-                </template>
-                <template #head(1)="">
-                    <span class="text-info">Uptime in a month %</span>
-                </template>
-                <template #cell(1)="data">
-                    <b-progress :value="data.value" :max="1" show-progress animated></b-progress>
+    <b-table 
+    sticky-header 
+    :items="itemsuptime" 
+    :fields="fieldsuptime" 
+    head-variant="light"
+    :sort-by.sync="sortBy"
+    :sort-desc.sync="sortDesc"
+    >
+                <template #cell(status)="data">
+      <div v-if="data.value === 'up'" style="background-color: green; color: white;width: 50%;margin:0 auto;">
+        {{ data.value }}
+      </div>
+      <div v-if="data.value === 'down'" style="background-color: red; color: white;width: 50%;margin:0 auto;">
+        {{ data.value }}
+      </div>
+      <div v-if="data.value === 'blocked'" style="background-color: grey; color: white;width: 50%;margin:0 auto;">
+        {{ data.value }}
+      </div>
+    </template>
 
-                    <b-progress class="mt-2" :max="1" show-value>
-                        <b-progress-bar :value="data.value" variant="success"></b-progress-bar>
-                        <b-progress-bar :value="0.1" variant="warning"></b-progress-bar>
-                        <b-progress-bar :value="0.2" variant="danger"></b-progress-bar>
-                    </b-progress>
+                <template #cell(uptime_month)="data">
+                    <b-progress :value="data.value" :max="1" show-progress animated></b-progress>
                 </template>
             </b-table>
   </div>
@@ -24,55 +31,40 @@
   export default {
     name: "UpTime",
     data() {
-        return {
-            state_monthly_uptimes: []
-        }
+      return {
+        itemsuptime: [{},],
+        sortBy: 'uptime_month',
+        sortDesc: false,
+        fieldsuptime: [
+          {
+            key: 'metadata.state_id',
+            label: 'state'
+          },
+          {
+            key: 'status',
+            label: 'current status'
+          },
+          {
+            key: 'uptime_month',
+            label: 'uptime by month'
+          },
+        ],
+      }
     },
-    async mounted() {
-        const response = await fetch("https://uptime.voteamerica.com/v1/uptime/sites/?limit=395")
-        .then(results => { return results.json()});
-        this.services = response.results;
-        var states = {};
-        var uptimes = {};
-        // assign all sites to states 
-        // added states instead of just downtimes to allow further implementations
-        for (const index in this.services) {
-            const site = this.services[index];
-            if (site.metadata != null) {
-                const state_id = site.metadata.state_id;
-                if (state_id in states) {
-                    states[state_id].push(site);
-                } else {
-                    states[state_id] = [site];
-                }
-            }
-        }
-        // average downtimes for each state 
-        for (const state in states) {
-            const sites = states[state];
-            var uptime = {'uptime_day': 0, 'uptime_month': 0, 'uptime_quarter': 0, 'uptime_week': 0};
-            for (const site_index in sites) {
-                const site = sites[site_index];
-                uptime['uptime_day'] += site.uptime_day;
-                uptime['uptime_month'] += site.uptime_month;
-                uptime['uptime_quarter'] += site.uptime_quarter;
-                uptime['uptime_week'] += site.uptime_week;
-            }
-            uptime['uptime_day']/=sites.length;
-            uptime['uptime_month']/=sites.length;
-            uptime['uptime_quarter']/=sites.length;
-            uptime['uptime_week']/=sites.length;
-            uptimes[state] = uptime;
-        }
-        // sort states by monthly uptime
-        var state_monthly_uptimes = Object.keys(uptimes).map(function(key) {
-            return [key, uptimes[key]['uptime_month']];
-        });
-
-        state_monthly_uptimes.sort(function(first, second) {
-            return first[1]-second[1];
-        });
-        this.state_monthly_uptimes = state_monthly_uptimes;
-    },
+    mounted() {
+    this.fetchData().catch(error => {
+      console.error(error)
+    })
+  },
+  methods: {
+    async fetchData() {
+      this.itemsuptime = await fetch("https://uptime.voteamerica.com/v1/uptime/sites/?limit=395")
+        .then(res => {
+          return res.json()
+        })
+        .then(itemsuptime => itemsuptime.results)
+        console.log(this.itemsuptime);
+    }
+  },
   }
 </script>
